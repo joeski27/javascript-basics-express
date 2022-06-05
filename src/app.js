07088ffaafb9748bd56121f086d5a21f0bc83661
Joseph Smith <jsmith2799@hotmail.com>
2022-06-05 17:08:49 +0100

    appfilecompmaybe

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const numbersRoute = require('./routes/numbers-route');
 const stringsRoute = require('./routes/strings-route');
+const arraysRoute = require('./routes/arrays-route');
+const booleansRoute = require('./routes/booleans-route');
 
 const app = express();
 
 app.use(express.json());
 app.use('/numbers', numbersRoute);
 app.use('/strings', stringsRoute);
-
+app.use('/arrays', arraysRoute);
+app.use('/booleans', booleansRoute);
 module.exports = app;

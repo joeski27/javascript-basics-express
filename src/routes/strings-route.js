6f28fa6d3968510f9ced689b9b05e077941ea6a2
Joseph Smith <jsmith2799@hotmail.com>
2022-06-05 12:27:48 +0100

    allstrings

--- src/routes/strings-route.js
+++ src/routes/strings-route.js
@@ -11,5 +11,19 @@ const {
 router.get('/hello/:string', (req, res) => {
   res.status(200).json({ result: sayHello(req.params.string) });
 });
+router.get('/upper/:string', (req, res) => {
+  res.status(200).json({ result: uppercase(req.params.string) });
+});
 
+router.get('/lower/:string', (req, res) => {
+  res.status(200).json({ result: lowercase(req.params.string) });
+});
+
+router.get('/first-characters/:string', (req, res) => {
+  const nCharacters = req.query.length;
+  res.status(200);
+  return nCharacters
+    ? res.json({ result: firstCharacters(req.params.string, nCharacters) })
+    : res.json({ result: firstCharacter(req.params.string) });
+});
 module.exports = router;

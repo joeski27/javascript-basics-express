f21a64f35397ab1fc44290a2aab119b1cf2a468e
Joseph Smith <jsmith2799@hotmail.com>
2022-06-05 12:21:05 +0100

    correctedappfile

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+const stringsRoute = require('./routes/strings-route');
+
 const app = express();
+
+app.use(express.json());
+
+app.use('/strings', stringsRoute);
 
 module.exports = app;

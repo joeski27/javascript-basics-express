7c160c7143eb5a34d20bf5037e19a93fb262e02d
Joseph Smith <jsmith2799@hotmail.com>
2022-06-05 17:01:17 +0100

    appupdated

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const numbersRoute = require('./routes/numbers-route');
 const stringsRoute = require('./routes/strings-route');
 
 const app = express();
 
 app.use(express.json());
-
+app.use('/numbers', numbersRoute);
 app.use('/strings', stringsRoute);
 
 module.exports = app;

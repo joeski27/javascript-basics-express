76697db4119ddd49aeb672c76874b49cbf875160
Joseph Smith <jsmith2799@hotmail.com>
2022-06-05 12:08:56 +0100

    trialfyrirstrings

---
+++ src/routes/strings-route.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const {
+  sayHello,
+  uppercase,
+  lowercase,
+  firstCharacter,
+  firstCharacters,
+} = require('../lib/strings');
+
+router.get('/hello/:string', (req, res) => {
+  res.status(200).json({ result: sayHello(req.params.string) });
+});
